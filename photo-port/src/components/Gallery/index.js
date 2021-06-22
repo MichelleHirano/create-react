9fccfd4019e6b5810c6739ed6c832e59b8f663f4
MHirano <michelle@hiranohomes.com>
2021-06-22 12:10:49 -0700

    updating files paths

--- photo-port/src/components/Gallery/index.js
+++ photo-port/src/components/Gallery/index.js
@@ -1,50 +1,15 @@
 import React from 'react';
+import PhotoList from './PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav(props) {
-  const {
-    categories = [],
-    setCurrentCategory,
-    currentCategory,
-  } = props;
-
+function Gallery(props) {
+  const { currentCategory } = props;
   return (
-    <header className="flex-row px-1">
-      <h2>
-        <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
-        </a>
-      </h2>
-      <nav>
-        <ul className="flex-row">
-          <li className="mx-2">
-            <a data-testid="about" href="#about">
-              About me
-            </a>
-          </li>
-          <li className="mx-2">
-            <span>Contact</span>
-          </li>
-          {categories.map((category) => (
-            <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
-                }`}
-              key={category.name}
-            >
-              <span
-                onClick={() => {
-                  setCurrentCategory(category)
-                }}
-              >
-                {capitalizeFirstLetter(category.name)}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <section>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name} />
+    </section>
   );
 }
-
-export default Nav;
+export default Gallery;
